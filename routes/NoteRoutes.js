cdc922b3f714a0a27f520a619c9601e7a319afda
Miko <jm_mapilisan@yahoo.com>
2022-11-21 23:50:32 +0800

    add verifyjwt on every route

--- routes/NoteRoutes.js
+++ routes/NoteRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const { getAllNotes, createNewNote, updateNote, deleteNote } = require('../controllers/NoteControllers')
+const verifyJWT = require('../middleware/verifyJWT')
 const router = express.Router()
+
+router.use(verifyJWT)
 
 router.route('/')
     .get(getAllNotes)
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const { getAllUsers, createNewUser, updateUser, deleteUser } = require('../controllers/UserControllers')
+const verifyJWT = require('../middleware/verifyJWT')
 const router = express.Router()
+
+router.use(verifyJWT)
 
 router.route('/')
     .get(getAllUsers)
